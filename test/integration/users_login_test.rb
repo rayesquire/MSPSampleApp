ccf90e1f64e1c738246c681830f202968e751a57
mashaopeng <fantastic.msp@gmail.com>
2018-06-01 20:13:44 +0800

    update login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,6 +20,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # 模拟用户在另一个窗口中退出登录
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
@@ -34,5 +36,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    # assert_not_empty cookies['remember_token']
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,24 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-  include ApplicationHelper
+  class ActiveSupport::TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+    include ApplicationHelper
 
-  def is_logged_in?
-    !session[:user_id].nil?
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
   end
-end
+
+  class ActionDispatch::IntegrationTest
+    def log_in_as(user, password: 'password', remember_me: '1')
+      post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me } }
+    end
+  end
