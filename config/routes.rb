d6b7fd6e322da30022e9eccda14016e7972d360c
mario <mashaopeng@meituan.com>
2018-06-21 19:52:54 +0800

    add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   # post '/signup', to: 'users#create'
 
   resources :users
+  resources :account_activations, only: [:edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
